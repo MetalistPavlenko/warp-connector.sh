c6cb9f9aa0c4fbae139b68fc391a83548491bc03
Ihor <metalist.pavlenko@gmail.com>
2025-08-15 14:28:27 +0300

    Add files via upload

--- warp-connector.sh
+++ warp-connector.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clear
+
 echo -e "\n\n\nGenerating...\n"
 
 {
@@ -32,8 +34,10 @@ Address = $(jq -r .interface.v4 < /var/lib/cloudflare-warp/conf.json)/12,$(jq -r
 [Peer]
 PublicKey = $(jq -r .public_key < /var/lib/cloudflare-warp/conf.json)
 AllowedIPs = 100.96.0.0/12
-Endpoint = $(jq -r .endpoints[0].v4 < /var/lib/cloudflare-warp/conf.json),$(jq -r .endpoints[1].v4 < /var/lib/cloudflare-warp/conf.json),$(jq -r .endpoints[2].v4 < /var/lib/cloudflare-warp/conf.json),$(jq -r .endpoints[3].v4 < /var/lib/cloudflare-warp/conf.json)
-# Endpoint = $(jq -r .endpoints[0].v6 < /var/lib/cloudflare-warp/conf.json),$(jq -r .endpoints[1].v6 < /var/lib/cloudflare-warp/conf.json),$(jq -r .endpoints[2].v6 < /var/lib/cloudflare-warp/conf.json),$(jq -r .endpoints[3].v6 < /var/lib/cloudflare-warp/conf.json)
+Endpoint = $(jq -r .endpoints[0].v4 < /var/lib/cloudflare-warp/conf.json),$(jq -r .endpoints[0].v6 < /var/lib/cloudflare-warp/conf.json)
+# Endpoint = $(jq -r .endpoints[1].v4 < /var/lib/cloudflare-warp/conf.json),$(jq -r .endpoints[1].v6 < /var/lib/cloudflare-warp/conf.json)
+# Endpoint = $(jq -r .endpoints[2].v4 < /var/lib/cloudflare-warp/conf.json),$(jq -r .endpoints[2].v6 < /var/lib/cloudflare-warp/conf.json)
+# Endpoint = $(jq -r .endpoints[3].v4 < /var/lib/cloudflare-warp/conf.json),$(jq -r .endpoints[3].v6 < /var/lib/cloudflare-warp/conf.json)
 PersistentKeepalive = 25
 EOL
 
